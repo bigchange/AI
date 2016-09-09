7b0a30be6bdbb3aab4c2f52a1b99a7e9a2f9895d
shanghai-Jerry <cjyou20110920sspu@163.com>
2016-09-09 11:47:49 +0800

    add K nearest neighborhoods

--- src/main/scala/com/bigchange/datamining/DistanceRecommend.scala
+++ src/main/scala/com/bigchange/datamining/DistanceRecommend.scala
@@ -50,32 +50,55 @@ object DistanceRecommend {
   }
   // 皮尔逊相关系数: 相关性,它的值在-1至1之间，1表示完全吻合，-1表示完全相悖
   def pearson(rating1List:List[Rating], rating2List:List[Rating]): Double = {
-    // ??
-    0.0
+
+    val bothRatingList = new ListBuffer[(Double, Double)]
+    // 先筛选出有共同评分的项
+    rating1List.foreach(x => rating2List.foreach(y => if(x.item == y.item)  bothRatingList.+=((x.value, y.value))))
+
+    println("bothRatingList:" + bothRatingList)
+
+    val averageX = bothRatingList.map(_._1).sum / ( bothRatingList.size * 1.0 )
+    val averageY = bothRatingList.map(_._2).sum / ( bothRatingList.size * 1.0)
+    // 计算分子
+    val up = bothRatingList.map(x => (x._1 - averageX) * (x._2 - averageY)).sum
+    // 计算分母
+    val tmp = bothRatingList.map(x => (pow(x._1 - averageX, 2),pow(x._2 - averageY, 2)))
+    val x = pow(tmp.map(_._1).sum, 0.5)
+    val y = pow(tmp.map(_._2).sum, 0.5)
+
+    val down = x * y
+
+    up / down * 1.0
+
   }
 
   // 余弦相似度的范围从1到-1，1表示完全匹配，-1表示完全相悖
   def cosSim(vector1: DoubleMatrix, vector2: DoubleMatrix): Double = {
-
+    // 此处的用户评分向量应该是共有的部分
     vector1.dot(vector2) / (vector1.norm2 * vector2.norm2)
 
   }
 
   // 找出距离最近的用户（其实该函数会返回一个用户列表，按距离排序）
-  def computeNearestNeighbor(user:String, userList: Map[String,List[Rating]]): List[(String,Double)] = {
+  def computeNearestNeighbor(user:String, userList: Map[String,List[Rating]], metric: (List[Rating] , List[Rating]) => Double): List[(String,Double)] = {
 
     var list = new ListBuffer[(String,Double)]
-    userList.foreach(x => if(x._1 != user) list.+=((x._1, manhattan(userList(user),userList(x._1)))))
+    userList.foreach(x => if(x._1 != user) list.+=((x._1, metric(userList(user),userList(x._1)))))
     list.toList.sortBy(_._2)
 
   }
+
+  // 找出K最近邻用户，根据相似度的值给对应用户设定评分的决定权重，按照权重重新计算评分，找出最高评分。下面使用pearson系数确定K个邻近用户
+  def computeKNearestNeighbor(user:String, userList: Map[String,List[Rating]], metric: (List[Rating] , List[Rating]) => Double, k:Int) = computeNearestNeighbor(_,_,_).take(_)
 
   // recommend : 数据量大的话使用RDD
   def recommend(user:String, userList: Map[String,List[Rating]]): List[(String,Double)] = {
 
     var recommendList = new ListBuffer[(String,Double)]
+
+    val distanceComp = minkowski(_, _, 1)
     // 找到距离最近的用户
-    val nearest = computeNearestNeighbor(user, userList).head
+    val nearest = computeNearestNeighbor(user, userList, distanceComp).head
     // 自己评价的乐队
     val userItemList = userList(user).map(_.item).iterator.toList
     // 找出这位用户评价过
