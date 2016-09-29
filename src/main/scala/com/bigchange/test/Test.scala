80c43b425a0e8ab050ca4a02bca7876e8426dbb1
shanghai-Jerry <cjyou20110920sspu@163.com>
2016-09-29 14:01:33 +0800

    RowMatrix

--- src/main/scala/com/bigchange/test/Test.scala
+++ src/main/scala/com/bigchange/test/Test.scala
@@ -1,5 +1,7 @@
 package com.bigchange.test
 
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.linalg.distributed.RowMatrix
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
@@ -11,11 +13,32 @@ object Test {
 
   def main(args: Array[String]) {
 
-    val rdd2 = sc.makeRDD(Seq("A","B","R","D","F","G","H","I","J","K","L","M","N"),4)
+    // @Test1 index唯一性测试
+    // val rdd2 = sc.makeRDD(Seq("A","B","R","D","F","G","H","I","J","K","L","M","N"),4)
 
     // rdd2.zipWithIndex().foreach(println)
 
-    rdd2.zipWithUniqueId().foreach(println)
+    // rdd2.zipWithUniqueId().foreach(println)
+
+    // @Test 中心化和零均值中心化
+    val rdd = sc.makeRDD(Seq(Array(1.0,2.0,3.0),Array(4.0,5.0,6.0),Array(7.0,8.0,9.0)))
+
+    val vectors = rdd.map(x => Vectors.dense(x))
+
+    val rowMatrix = new RowMatrix(vectors)
+
+    /**
+      * [1.0,2.0,3.0]
+      * [4.0,5.0,6.0]
+      * [7.0,8.0,9.0]
+      */
+    rowMatrix.rows.foreach(println)
+
+    val means = rowMatrix.computeColumnSummaryStatistics().mean
+
+    println("means:"+ means)  // means:[4.0,5.0,6.0]
+
+
 
   }
 
