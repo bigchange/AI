9437f13d7583983c0327adeb598468b6e6921d13
shanghai-Jerry <cjyou20110920sspu@163.com>
2016-09-30 15:39:39 +0800

    Test

--- src/main/scala/com/bigchange/test/Test.scala
+++ src/main/scala/com/bigchange/test/Test.scala
@@ -1,11 +1,12 @@
 package com.bigchange.test
-
-import org.apache.spark.mllib.linalg.Vectors
-import org.apache.spark.mllib.linalg.distributed.RowMatrix
 import org.apache.spark.{SparkConf, SparkContext}
+
+import scala.util.Random
 
 /**
   * Created by C.J.YOU on 2016/9/27.
+  * 处理数据过程中
+  * 涉及到的一些转换的Test用例
   */
 object Test {
 
@@ -14,17 +15,17 @@ object Test {
   def main(args: Array[String]) {
 
     // @Test1 index唯一性测试
-    // val rdd2 = sc.makeRDD(Seq("A","B","R","D","F","G","H","I","J","K","L","M","N"),4)
+    /* val rdd2 = sc.makeRDD(Seq("A","B","R","D","F","G","H","I","J","K","L","M","N"),4)
 
-    // rdd2.zipWithIndex().foreach(println)
+     rdd2.zipWithIndex().foreach(println)
 
-    // rdd2.zipWithUniqueId().foreach(println)
+     rdd2.zipWithUniqueId().foreach(println)*/
 
     // @Test 中心化和零均值中心化
-    val rdd = sc.makeRDD(Seq(Array(1.0,2.0,3.0),Array(4.0,5.0,6.0),Array(7.0,8.0,9.0)))
+    /*val rdd = sc.makeRDD(Seq(Array(1.0,2.0,3.0),Array(4.0,5.0,6.0),Array(7.0,8.0,9.0)))
 
     val vectors = rdd.map(x => Vectors.dense(x))
-
+    // Array[Double]是所库中类的通用变量形式
     val rowMatrix = new RowMatrix(vectors)
 
     /**
@@ -37,6 +38,18 @@ object Test {
     val means = rowMatrix.computeColumnSummaryStatistics().mean
 
     println("means:"+ means)  // means:[4.0,5.0,6.0]
+
+    // 均值减法
+    val bVectors = rowMatrix.rows.map(x => breeze.linalg.DenseVector(x.toArray) - breeze.linalg.DenseVector(means.toArray))
+      .foreach(println)
+
+    val scaler = new StandardScaler(withMean = true, withStd = false).fit(vectors) // 提取mean
+    val scaledVectors = vectors.map { v => scaler.transform(v) } // 向量减去当前列的平均值 */
+
+    // @ Test 权重小随机数初始化
+    val random = new Random()
+    val dimension = 10
+    val weights = Array.tabulate(10)(_ => random.nextGaussian()) // 基于零均值和标准差的一个高斯分布（译者注：国内教程一般习惯称均值参数为期望\mu）来生成随机数的
 
 
 
