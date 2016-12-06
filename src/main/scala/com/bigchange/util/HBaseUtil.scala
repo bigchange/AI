cc2e8c8b17aab2f63b262e2bf508745e00fb721c
shanghai-Jerry <cjyou20110920sspu@163.com>
2016-12-06 16:40:20 +0800

    + update

--- src/main/scala/com/bigchange/util/HBaseUtil.scala
+++ src/main/scala/com/bigchange/util/HBaseUtil.scala
@@ -2,31 +2,34 @@ package com.bigchange.util
 
 import com.bigchange.log.CLogger
 import org.apache.hadoop.hbase.client._
-import org.apache.hadoop.hbase.util.Bytes
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable
+import org.apache.hadoop.hbase.mapreduce.TableInputFormat
+import org.apache.hadoop.hbase.protobuf.ProtobufUtil
+import org.apache.hadoop.hbase.protobuf.generated.ClientProtos
+import org.apache.hadoop.hbase.util.{Base64, Bytes}
 import org.apache.hadoop.hbase.{HBaseConfiguration, HColumnDescriptor, HTableDescriptor, TableName}
+import org.apache.spark.SparkContext
 
 import scala.collection.mutable
-
-
 
 /**
   * Created by C.J.YOU on 2016/3/21.
   */
 object HBaseUtil {
 
+  private val hBaseConfiguration = getConfiguration
+
   private lazy val connection = getConnection
 
   private case class RowData(rowKey: String, dataMap:mutable.HashMap[String, String])
 
-  // 自定义外部可访问接口：存储对应格式数据到hbase即可
+  // 自定义外部可访问接口：存储对应格式数据到hbase即可（部分对象 特征 可根据实际需求 再进行一层抽象）
 
-  /**
-    * 获取hbase的连接器
-    * @return connection
-    */
-  private def getConnection: Connection ={
+  private def getConfiguration = {
 
+    // 本地测试 需配置的选项， 在集群上通过对应配置文件路径自动获得
     val hbaseConf = HBaseConfiguration.create
+
     hbaseConf.set("fs.defaultFS", "hdfs://ns1"); // nameservices的路径
     hbaseConf.set("dfs.nameservices", "ns1");  //
     hbaseConf.set("dfs.ha.namenodes.ns1", "nn1,nn2"); //namenode的路径
@@ -37,9 +40,20 @@ object HBaseUtil {
     hbaseConf.set("hbase.rootdir", "hdfs://ns1/hbase")
     hbaseConf.set("hbase.zookeeper.quorum", "server0,server1,server2")
     hbaseConf.set("hbase.zookeeper.property.clientPort", "2181")
+
+    hbaseConf
+
+  }
+  /**
+    * 获取hbase的连接器
+    * @return connection
+    */
+  private def getConnection: Connection = {
+
     CLogger.warn("create connection")
 
-    val connection = ConnectionFactory.createConnection(hbaseConf)
+    val connection = ConnectionFactory.createConnection(hBaseConfiguration)
+
     sys.addShutdownHook {
       connection.close ()
     }
@@ -130,6 +144,50 @@ object HBaseUtil {
     }
 
     true
+
+  }
+
+  /**
+    * 通过rowKey 获取数据
+    * @param rowKey  键
+    * @param tableName 表名
+    * @param family 列族
+    * @param qualifiers 列名集合
+    * @return Result of get
+    */
+  def get(rowKey: String, tableName:String, family: String, qualifiers:Array[String]) = {
+
+    val htable = connection.getTable(TableName.valueOf(tableName))
+    val get = new Get(Bytes.toBytes(rowKey))
+
+    for (qualifier <- qualifiers) {
+      get.addColumn(Bytes.toBytes(family), Bytes.toBytes(qualifier))
+    }
+
+    htable.get(get)
+
+  }
+
+  /**
+    * 通过rdd 形式批量获取hbase数据
+    * @param sc sparkContext
+    * @param property 获取数据的属性：tableName, family, qualifiers , ...
+    * @param scan  scan 定义
+    * @return  RDD[Result]
+    */
+  def getHbaseDataThroughNewAPI(sc: SparkContext, property: Map[String, String], scan: Scan) = {
+
+    val proto:ClientProtos.Scan = ProtobufUtil.toScan(scan)
+    val scanToString = Base64.encodeBytes(proto.toByteArray)
+    hBaseConfiguration.set(TableInputFormat.SCAN, scanToString)
+
+    property.foreach { case(key, value) =>
+      hBaseConfiguration.set(key, value)
+    }
+
+    val hbaseRDD = sc.newAPIHadoopRDD(hBaseConfiguration, classOf[TableInputFormat], classOf[ImmutableBytesWritable], classOf[Result]).map(_._2)
+
+    hbaseRDD
 
   }
 
