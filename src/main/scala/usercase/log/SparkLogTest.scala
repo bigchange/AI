e07a45e624b00ce8cd5c62b300e49a9fd3a7b626
shanghai-Jerry <cjyou20110920sspu@163.com>
2016-12-15 16:02:22 +0800

    + update

---
+++ src/main/scala/usercase/log/SparkLogTest.scala
@@ -0,0 +1,38 @@
+package usercase.log
+
+import com.bigchange.log.CLogger
+import com.bigchange.spark.Spark
+
+import scala.util.{Failure, Success, Try}
+
+/**
+  * Created by C.J.YOU on 2016/12/15.
+  */
+object SparkLogTest extends CLogger{
+
+  val sc = Spark.apply("local","SparkLogTest", 60).sc
+
+  def main(args: Array[String]) {
+
+    val Array(path,logFile) = args
+
+    logConfigure(logFile)
+
+    val data = Try(sc.textFile(path)) match {
+
+      case Success(r) =>
+        info("count:" + r.count())
+      case Failure(exception) =>
+        error("this is test error log")
+        warn("this is warning log")
+        info("this is info log")
+        debug("this is debug log")
+        errorLog(logFileInfo, "this is errorLog")
+    }
+
+
+
+
+  }
+
+}
---
+++ src/main/scala/usercase/telecom/Test.scala
@@ -0,0 +1,15 @@
+package usercase.telecom
+
+/**
+  * Created by C.J.YOU on 2016/12/15.
+  */
+object Test {
+
+
+  def main(args: Array[String]) {
+
+    SHRealTimeSearchAndVisitCount.usingMysqlHandler()
+
+  }
+
+}
